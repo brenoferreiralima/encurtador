2db6063fb43297ed32fbb9973cc1fff82481c903
Breno Lima <brenoferreiralima@gmail.com>
2022-09-27 19:12:40 +0000

    updated app.py

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests, json
+
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    link = encurtador("https://www.encurtador.dev/")
-    return render_template("index.html", link=link)
+    return render_template("index.html")
 
 
 # recebe uma URL e retorna uma URL encurtada
-def encurtador(url):
-    url = url
-    # url da api do encurtador
-    api = 'https://api.encurtador.dev/encurtamentos'
-    # json que será enviado a API
-    payload = {'url':url}
-    # chave que contém o valor da url encurtada
-    key = 'urlEncurtada'
+@app.route("/encurtador", methods=["GET","POST"])
+def encurtador():
+    if request.method == "POST":
+        # url original
+        url  = request.form["url"]
+        # url da api do encurtador
+        api = 'https://api.encurtador.dev/encurtamentos'
+        # json que será enviado a API
+        payload = {'url':url}
+        # chave que contém o valor da url encurtada
+        key = 'urlEncurtada'
 
-    result = requests.post(api, json=payload)
-    if result.status_code == 200:
-        result = result.json()
-        result = result[key]
-    
-    return result
+        result = requests.post(api, json=payload)
+        if result.status_code == 200:
+            result = result.json()
+            result = result[key]
+        
+    return render_template("encurtador.html",link=result)
